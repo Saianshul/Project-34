e95c6f88f6572266c438e80a33355b8478846381
Saianshul <70786706+Saianshul@users.noreply.github.com>
2020-11-27 19:42:44 -0700

    Add files via upload

---
+++ js/pendulum.js
@@ -0,0 +1,30 @@
+class Pendulum {
+    constructor(x, y, r) {
+        var options = {
+            'restitution': 1,
+            'friction': 0,
+            'frictionAir': 0.0,
+            'slop': 1,
+            'inertia': Infinity
+        }
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.body = Bodies.circle(x, y, this.r, options);
+        World.add(world, this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        strokeWeight(2);
+        stroke("white");
+        fill("black");
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r, this.r);
+        pop();
+    }
+}
